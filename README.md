1ea18f83ef7dc2a5b2d25fa412197deef0400022
Eduardo Calle <49166547+eduardokalle@users.noreply.github.com>
2021-06-16 01:14:54 -0500

    first commit add api crud mysql

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # APIProject
 Api project base
+
+##Technologies
+node.js
+express
+sequelize
+cors
+sql
+mysql
+
+
+##Instalation 
+
+$ git clone https://example.com
+$ cd ../path/to/the/file
+$ npm install
+$ npm start
---
+++ app/config/db.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    HOST: "bndwb2qg61wnawcpxrc4-mysql.services.clever-cloud.com",
+    USER: "utgxorl5xmkzzpkm",
+    PASSWORD: "kIUqLCYXcKdnVAj6m3tO",
+    DB: "bndwb2qg61wnawcpxrc4",
+    dialect: "mysql",
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+  };
